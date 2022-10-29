25e1adfa7aae0627e189aa651ed07cdfd0667603
SharobiddinovNurmuhammad <102137376+SharobiddinovNurmuhammad@users.noreply.github.com>
2022-10-29 17:38:48 +0500

    This is comment

--- lesson-1/temp.py
+++ lesson-1/temp.py
@@ -1,6 +1,9 @@
 import tkinter as tk
 window = tk.Tk()
-window.title("Nurmuhammad")
-window.geometry('300x400')
-window.config(bg = '#001556')
+window.title("Calculator")
+window.geometry('300x400+0+0')
+window.config(bg = '#001919')
+#window.resizable(False,False)
+window.minsize(100,200)
+window.maxsize(500,600)
 window.mainloop()
