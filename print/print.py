64aa80d0d12e6015b4f0d6fe66f49f896db5dfc1
SharobiddinovNurmuhammad <102137376+SharobiddinovNurmuhammad@users.noreply.github.com>
2022-10-30 14:46:38 +0500

    This is comment

--- print/print.py
+++ print/print.py
@@ -1,2 +1,17 @@
 # print()
-print('Hello World')
+'''
+
+% qoldiqli bo'lish
+// bo'lish va bo'linmaning butun qismini olish
+** eksponenta (daraja/ildiz)
+
+'''
+print('"Nexia", "Tico", \'Damas\' ko\'rganlar qilara havas')
+
+print("5 ning 4-darajasi", 5**4)
+print("22 ni 4 ga bo'lgandagi qoldiq", 22%4)
+print("Tomonlari 125 ga teng kvadrat yuzi:",125**2,"perimetri:", 125*4)
+
+print("Diametri 12 ga teng doira yuzi:", 3.14*(12/2)**2)
+
+print("Katetlari 6 va 7 ga teng to'g'ri burchakli uchburchakning gipotenuzasi:",(6**2 + 7**2)**(1/2))
