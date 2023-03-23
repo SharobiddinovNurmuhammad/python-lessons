225da9ba8331bd363db155b482440e8d1111f504
Your Name <sharobiddinovnurmuhammad23@gmail.com>
2023-03-23 06:22:29 +0500

    1-dars qo'shildi

---
+++ 2-dars/2-amaliyot.py
@@ -0,0 +1,24 @@
+#O'zgaruvchilar
+
+#------------------------------------------------
+
+message = "Hello, World!"
+print(message)
+
+#------------------------------------------------
+
+xabar = "Assalomu alaykum, Xush kelibsiz!"
+print(xabar)
+
+#------------------------------------------------
+
+#class deb nomlangan o'zgaruvchi yaratib bo'lmaydi 
+#chunki, class maxsus kalit so'z hisoblanadi
+
+#------------------------------------------------
+
+radius = 5
+pi = 3.1415
+yuza = pi * radius**2
+print("Radiusi", radius, "ga teng aylana yuzi =", yuza)
+
