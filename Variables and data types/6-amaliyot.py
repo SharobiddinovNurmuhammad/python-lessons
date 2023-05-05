18417c755e0a6ec3e118ce69327d02a4a7c4135b
Your Name <sharobiddinovnurmuhammad23@gmail.com>
2023-05-06 00:08:07 +0500

    2-dars qo'shildi

---
+++ Variables and data types/6-amaliyot.py
@@ -0,0 +1,21 @@
+# Ro'yhat(List)lar ustida amallar
+
+#-----------------------------------------------
+
+country = ['Uzbekistan', 'Russia', 'Belarus', 'America', 'France', 'Kazakhistan']
+print(country)
+print(len(country))
+
+#-----------------------------------------------
+
+print("Tartiblangan ro'yhat:", sorted(country))
+print("Tartiblangan teskari ro'yhat:", sorted(country, reverse=True))
+print("Asl ro'yhat:", country)
+
+#-----------------------------------------------
+
+country.reverse()
+print(country)
+
+#-----------------------------------------------
+
